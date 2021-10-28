53ba066ab7c7e2f17d70073f4a5b800f823a24e0
Ralf Lang <lang@b1-systems.de>
2021-10-28 06:19:07 +0000

    Straight conversion to namespaced equivalent.
    Only minor changes to type hints and parameter types.
    Unit tests are still for the unnamespaced version.

---
+++ src/Exception.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Horde Routes package
+ *
+ * This package is heavily inspired by the Python "Routes" library
+ * by Ben Bangert (http://routes.groovie.org).  Routes is based 
+ * largely on ideas from Ruby on Rails (http://www.rubyonrails.org).
+ *
+ * @author  Maintainable Software, LLC. (http://www.maintainable.com)
+ * @author  Mike Naberezny <mike@maintainable.com>
+ * @license http://www.horde.org/licenses/bsd BSD
+ * @package Routes
+ */
+namespace Horde\Routes;
+use \Horde_Exception_Wrapped;
+/**
+ * Exception class for the Horde\Routes package.  All exceptions thrown
+ * from the package will be of this type.
+ * 
+ * @package Routes
+ */
+class Exception extends Horde_Exception_Wrapped
+{
+}
